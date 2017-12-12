c93eb0210df63d17ec9914b7af6900db225b5650
Calam Carroll <calamcarroll@gmail.com>
2017-12-12 14:27:57 +0000

    First stage of tests for programs page

--- test/acceptance/programs-test.js
+++ test/acceptance/programs-test.js
@@ -19,43 +19,33 @@ test.describe('Programs page', function() {
         pageSelector = By.id('programs');
     } );
     test.beforeEach( function() {
-        driver.get('http://localhost:3000/#/donations');
+        driver.get('http://localhost:3000/#/programs');
         driver.wait(until.elementLocated(pageSelector), 2000);
     } );
 
     test.it( 'shows the main header', function() {
         driver.findElement(By.tagName('h1')).then( function( element ) {
             element.getText().then(function(text) {
-                expect(text).to.equal('List All Donations');
+                expect(text).to.equal('List All programs');
             });
         });
     } );
 
-    test.it( 'displays the donations', function() {
-        var donations = driver.findElements(By.tagName('tr'));
-        // 1st donation amount should be 1100
-        donations
-            .then(function( elements ) {
-                return elements[0].findElement(By.name('amount'));
-            })
-            .then(function(element) {
-                return element.getText();
-            })
-            .then(function(text) {
-                expect(text).to.equal('1100');
-            } );
-        // 2nd donation amount should be 1600
-        donations
-            .then( function( elements ) {
-                return elements[1].findElement(By.name('amount'));
-            })
-            .then(function(element) {
-                return element.getText();
-            })
-            .then(function(text) {
-                expect(text).to.equal('1600');
-            } );
-    } );
+    // test.it( 'Displays all programs', function() {
+    //     var programs = driver.findElements(By.tagName('td'));
+    //
+    //     programs
+    //         .then(function( elements ) {
+    //             return elements[0].findElement(By.name('Sets'));
+    //         })
+    //         .then(function(element) {
+    //             return element.getText();
+    //         })
+    //         .then(function(text) {
+    //             expect(text).to.equal('5');
+    //         } );
+    //
+    // } );
 
     test.afterEach( function() {
         driver.manage().deleteAllCookies() ;
